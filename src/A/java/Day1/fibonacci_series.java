f719a01643eef4a3048a4bbec8b3cf905277c470
Harish1138 <harya0102@gmail.com>
2025-08-03 17:19:14 +0530

    update fibonacci_series

--- src/A/java/Day1/fibonacci_series.java
+++ src/A/java/Day1/fibonacci_series.java
@@ -1,5 +1,25 @@
 package A.java.Day1;
 import java.util.*;
 public class fibonacci_series {
+    public static void isFib(int n) {
+        int a=0;
+        int b=1;
+        int c=0;
+        System.out.print(a+" "+b+" ");
+        for(int i=0;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            System.out.print(c+" ");
+        }
+//        return c;
+    }
 
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+
+        isFib(n);
+
+    }
 }
