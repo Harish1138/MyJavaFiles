9505ba0ccfb1c8e88b0aa978ad5cb9b34f10bdf4
Harish1138 <harya0102@gmail.com>
2025-08-07 00:29:45 +0530

    intial commit

---
+++ src/A/java/Day1/sum.java
@@ -0,0 +1,18 @@
+package A.java.Day1;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int a;
+        int b;
+        System.out.print("Enter the value of a: ");
+        a=input.nextInt();
+        System.out.print("Enter the value of b: ");
+        b=input.nextInt();
+        int sum=a+b;
+        System.out.println("Total sum of a and b is: "+sum);
+    }
+}
