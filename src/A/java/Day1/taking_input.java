deefa6454117a036cc6f4408bd71b6c8f7edd243
Harish1138 <harya0102@gmail.com>
2025-08-07 00:25:52 +0530

    intial commit

---
+++ src/A/java/Day1/taking_input.java
@@ -0,0 +1,12 @@
+package A.java.Day1;
+import java.util.*;
+public class taking_input {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n;
+        System.out.print("Enter the number  by your choice: ");
+        n=input.nextInt();
+        System.out.print("Number that you enteres is: "+n);
+
+    }
+}
