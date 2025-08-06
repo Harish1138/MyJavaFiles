74c844b4c44a2e5354f83c94481ae97bf71f5402
Harish1138 <harya0102@gmail.com>
2025-08-07 00:10:22 +0530

    intial commit

---
+++ src/A/java/Day1/palindrome_number.java
@@ -0,0 +1,22 @@
+package A.java.Day1;
+import java.util.*;
+// Palindrome Number Without function
+public class palindrome_number {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n=input.nextInt();
+
+        int m=n;
+        int rem=0;
+        int rev=0;
+        while(n>0){
+            rem=n%10;
+            n=n/10;
+            rev=rev*10+rem;
+        }
+        System.out.println(rev);
+
+
+    }
+}
