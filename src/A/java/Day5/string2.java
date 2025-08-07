a0d41c79a71ccbd836c9a56846d3813a41417c75
Harish1138 <harya0102@gmail.com>
2025-08-07 12:20:55 +0530

    intial commit

---
+++ src/A/java/Day5/string2.java
@@ -0,0 +1,29 @@
+package A.java.Day5;
+
+import java.util.*;
+
+public class string2 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        StringBuilder stb=new StringBuilder();
+        int lineCount=0;
+        while(input.hasNextLine()){
+            String line=input.nextLine();
+            if(line.equals("END")){
+                break;
+            }
+            stb.append(line);
+            stb.append("\n");
+            lineCount++;
+
+        }
+        String text=stb.toString();
+        int charCount=text.length();
+        int wordsCount=new StringTokenizer(text).countTokens();
+        System.out.println("Number of characters: "+charCount);
+        System.out.println("Number of lines: "+lineCount);
+        System.out.println("Number of wors: "+wordsCount);
+
+    }
+
+}
