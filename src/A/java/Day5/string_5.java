62f729d6edaa332ac268d0d38ecde58c46535510
Harish Kumar <harya0102@gmail.com>
2025-08-10 17:54:22 +0530

    Add files via upload

---
+++ src/A/java/Day5/string_5.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class string_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        String str = sc.nextLine();
+        StringBuilder stb = new StringBuilder(str);
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                stb.setCharAt(i,Character.toUpperCase(ch));
+            } else if (ch >= 'A' && ch <= 'Z') {
+                stb.setCharAt(i,Character.toLowerCase(ch));
+            } else {
+                // For non-alphabet characters, you can choose to append as-is or ignore
+                stb.append(ch);
+            }
+        }
+        System.out.println(stb.toString());
+    }
+}
